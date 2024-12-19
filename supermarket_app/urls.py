bcaa0712452eb08b80b985dd7467371adf5bd09b
satish24011993 <satishkurakula073@gmail.com>
2024-12-19 20:04:21 +0530

    update, view all, delete methods using restframeworks

--- supermarket_app/urls.py
+++ supermarket_app/urls.py
@@ -4,4 +4,7 @@ from supermarket_app import views
 urlpatterns = [
     path('/',views.ApiOverview, name='home'),
     path('/create/', views.add_item, name='create'),
+    path('/list/',views.all_items,name='all_items'),
+    path('/update/<int:id>',views.update, name='update'),
+    path('/item/delete/<int:id>', views.delete, name='delete')
 ]
--- supermarket_app/views.py
+++ supermarket_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Item
@@ -9,7 +9,7 @@ from rest_framework import serializers, status
 @api_view(['GET'])
 def ApiOverview(request):
     api_urls = {
-        'all_items':'/',
+        'all_items':'/list/',
         'Search by Category':'/?category=category_name',
         'Search by Sub Category':'/?subcategory=subcategory_name',
         'Delete Item':'/item/pk/delete',
@@ -34,5 +34,36 @@ def add_item(request):
         return Response(status=status.HTTP_400_NOT_FOUND)
 
 
-# @api_view(['GET'])
-# def 
+@api_view(['GET'])
+def all_items(request):
+    item = None
+
+    if request.query_params:
+        item = Item.objects.filter(**request.query_params.dict())
+    else:
+        item = Item.objects.all()
+    if item:
+        items_list = ItemSerializer(item, many=True)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(items_list.data)
+
+
+@api_view(['POST'])
+def update(request,id):
+    item_data = Item.objects.get(id=id)
+    update_item = ItemSerializer(instance = item_data,data=request.data)
+
+    if update_item.is_valid():
+        update_item.save()
+        return Response(update_item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def delete(request, id):
+    item_data = get_object_or_404(Item,id = id)
+    item_data.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
+    
